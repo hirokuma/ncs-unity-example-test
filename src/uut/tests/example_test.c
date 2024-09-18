9af2a85cf41a40d2ece8f20fa6189604b091554b
hirokuma <hiro9ma@gmail.com>
2024-09-18 16:04:18 +0900

    だめ

--- src/uut/tests/example_test.c
+++ src/uut/tests/example_test.c
@@ -38,16 +38,3 @@ void test_uut_init_with_param_check(void)
 	err = uut_init(NULL);
 	TEST_ASSERT_EQUAL(-1, err);
 }
-
-/* It is required to be added to each test. That is because unity's
- * main may return nonzero, while zephyr's main currently must
- * return 0 in all cases (other values are reserved).
- */
-extern int unity_main(void);
-
-int main(void)
-{
-	(void)unity_main();
-
-	return 0;
-}
---
+++ tests/test_main.c
@@ -0,0 +1,14 @@
+#include <unity.h>
+
+/* It is required to be added to each test. That is because unity's
+ * main may return nonzero, while zephyr's main currently must
+ * return 0 in all cases (other values are reserved).
+ */
+extern int unity_main(void);
+
+int main(void)
+{
+	(void)unity_main();
+
+	return 0;
+}
